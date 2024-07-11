bdcd3d64859d6ed6f8f3b7d585fb4096a1bf9e1e
Ragnarom-kun <romi.fadhurrohman@gmail.com>
2024-07-11 23:06:08 +0700

    feat: init swift ios project

---
+++ AlertSystem/AlertSystem/AlertSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSystemApp.swift
+//  AlertSystem
+//
+//  Created by Romi Fadhurohman Nabil on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
