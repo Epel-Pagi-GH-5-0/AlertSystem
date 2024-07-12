eac7a2d16566036fc5e89edd78e80167a3e80a4f
joshuawenata <joshuawenata@gmail.com>
2024-07-12 09:34:01 +0700

    feat: google-sign-in

--- AlertSystem/AlertSystemApp.swift
+++ AlertSystem/AlertSystemApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct AlertSystemApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ AlertSystem/View Model/FirebaseViewModel.swift
@@ -0,0 +1,57 @@
+import UIKit
+import Firebase
+import FirebaseAuth
+import GoogleSignIn
+
+func firebaseAuthConfig() {
+    FirebaseApp.configure()
+}
+
+func signInWithGoogle(presentingViewController: UIViewController, completion: @escaping (Result<User, Error>) -> Void) {
+    guard let clientID = FirebaseApp.app()?.options.clientID else {
+        completion(.failure(NSError(domain: "SignInError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to get client ID"])))
+        return
+    }
+
+    // Create Google Sign-In configuration object.
+    let config = GIDConfiguration(clientID: clientID)
+    GIDSignIn.sharedInstance.configuration = config
+
+    // Start the sign-in flow!
+    GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) { result, error in
+        if let error = error {
+            completion(.failure(error))
+            return
+        }
+
+        guard let user = result?.user,
+              let idToken = user.idToken?.tokenString else {
+            completion(.failure(NSError(domain: "SignInError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to retrieve user, idToken, or accessToken"])))
+            return
+        }
+
+        let accessToken = user.accessToken.tokenString
+
+        let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+
+        // Authenticate with Firebase using the Google credentials
+        Auth.auth().signIn(with: credential) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let user = authResult?.user else {
+                completion(.failure(NSError(domain: "SignInError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Firebase sign-in succeeded, but user object is nil"])))
+                return
+            }
+
+            completion(.success(user))
+        }
+    }
+}
+
+func logout() async throws {
+    GIDSignIn.sharedInstance.signOut()
+    try Auth.auth().signOut()
+}
